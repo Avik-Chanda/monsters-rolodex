f6ae7df1032207f8c5627e1883e81ec96ffa0915
Avik Chanda <avik.chanda@impelsys.com>
2019-08-16 15:24:38 +0530

    Updates

---
+++ precache-manifest.b53b755cff4ff38a895d0f3ce0281b12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e65a3a0fd8aadeb4faf4c1b5397cca8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4f0b7720209ca389e751",
+    "url": "/monsters-rolodex/static/css/main.b1b4f232.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "4f0b7720209ca389e751",
+    "url": "/monsters-rolodex/static/js/main.a3956cd0.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
